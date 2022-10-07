f014b08c2027db2abcd9a4b7a5e5464ffa0b387b
Masouma1 <105571511+Masouma1@users.noreply.github.com>
2022-10-07 21:55:26 +0430

    fix linter error

--- src/app.js
+++ src/app.js
@@ -1,102 +1,107 @@
 import './style.css';
-const addContent = document.querySelector("#formSection");
-const todoInput = document.querySelector("#newTodo");
-const notificationEl = document.querySelector(".notification");
+
+const addContent = document.querySelector('#formSection');
+const todoInput = document.querySelector('#newTodo');
+// dynamic todo list
+const todoListEl = document.querySelector('#todo-list');
 
 let todos = JSON.parse(localStorage.getItem('todos')) || [];
-window.addEventListener('load',()=>{
-    renderTodo();
-})
-//renderTodo();
-//form submit
-addContent.addEventListener("submit", function (event) {
-  event.preventDefault();
-  saveTodo();
-  renderTodo();
-  localStorage.setItem("todos", JSON.stringify(todos));
-});
 
-//save todo
+// save todo
 function saveTodo() {
   const todoValue = todoInput.value;
 
-  //check if todo id empty
-  const isEmpty = todoValue === "";
-  //check if duplicat
+  // check if todo id empty
+  const isEmpty = todoValue === '';
+  // check if duplicat
   const isDuplicate = todos.some(
-    (todo) => todo.value.toUpperCase() === todoValue.toUpperCase()
+    (todo) => todo.value.toUpperCase() === todoValue.toUpperCase(),
   );
 
   if (isEmpty) {
-    alert("input a todo");
+    alert('input a todo');
   } else if (isDuplicate) {
-    alert("Todo Already Exist");
+    alert('Todo Already Exist');
   } else {
     const todo = {
       value: todoValue,
       checked: false,
-      color: "#" + Math.floor(Math.random() * 16777215).toString(16),
+      color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
     };
     todos.push(todo);
-    todoInput.value = "";
+    todoInput.value = '';
     console.log(todos);
   }
 }
-
-//dynamic todo list
-const todoListEl = document.querySelector("#todo-list");
-
-//render todo
+// renderTodo();
+// render todo
 function renderTodo() {
-  //clear befor a re render
-  todoListEl.innerHTML = "";
+  // clear befor a re render
+  todoListEl.innerHTML = '';
   todos.forEach((todo, index) => {
     todoListEl.innerHTML += `
     <div class="todo" id=${index}>
      <i 
-     class="bi ${todo.checked ? "bi-check-lg" : "bi-circle"}"
+     class="bi ${todo.checked ? 'bi-check-lg' : 'bi-circle'}"
       style="color:${todo.color}"
       data-action="check"
       >
       </i> 
-     <p class="${todo.checked ? "checked" : ""}" data-action="check">${
-      todo.value
-    }</p> 
+     <p class="${todo.checked ? 'checked' : ''}" data-action="check">${
+  todo.value
+}</p> 
     <i class="bi bi-trash" data-action="delete"></i> 
 </div>`;
   });
 }
-//click event listener for all todos
-todoListEl.addEventListener("click", (event) => {
-  const target = event.target;
-  const parentElement = target.parentNode;
-  if (parentElement.className !== "todo") return;
-
-  //todo id
-  const todo = parentElement;
-  const todoId = Number(todo.id);
-
-  //target action
-  const action = target.dataset.action;
-
-  action === "check" && checkTodo(todoId);
-  action === "delete" && deleteTodo(todoId);
+// form submit
+addContent.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // eslint-disable-line no-use-before-define
+  saveTodo();
+  renderTodo();
+  localStorage.setItem('todos', JSON.stringify(todos));
 });
-//check to do
+
+// check to do
 function checkTodo(todoId) {
   todos = todos.map((todo, index) => ({
     ...todo,
     checked: index === todoId ? !todo.checked : todo.checked,
   }));
   renderTodo();
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-//delete to do
+// delete to do
 function deleteTodo(todoId) {
   todos = todos.filter((todo, index) => index !== todoId);
 
-  //re render
+  // re render
   renderTodo();
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
+
+// click event listener for all todos
+todoListEl.addEventListener('click', (event) => {
+  const { target } = event;
+  const parentElement = target.parentNode;
+  if (parentElement.className !== 'todo') return;
+
+  // todo id
+  const todo = parentElement;
+  const todoId = Number(todo.id);
+
+  // target action
+  const { action } = target.dataset;
+
+  if (action === 'check') {
+    checkTodo(todoId);
+  }
+  if (action === 'delete') {
+    deleteTodo(todoId);
+  }
+});
+window.addEventListener('load', () => {
+  renderTodo();
+});
