fbdeb51e4c2fa9203865db3ffb7ece2ace925c5c
Masouma1 <105571511+Masouma1@users.noreply.github.com>
2022-10-08 17:57:58 +0430

    Add new js file and import it to app.js

--- src/app.js
+++ src/app.js
@@ -1,66 +1,41 @@
-import './style.css';
+import "./style.css";
+import saveTodo from "./modules/saveTodo.js";
 
-const addContent = document.querySelector('#formSection');
-const todoInput = document.querySelector('#newTodo');
+const addContent = document.querySelector("#formSection");
+
 // dynamic todo list
-const todoListEl = document.querySelector('#todo-list');
+const todoListEl = document.querySelector("#todo-list");
 
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
+export let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-// save todo
-function saveTodo() {
-  const todoValue = todoInput.value;
-
-  // check if todo id empty
-  const isEmpty = todoValue === '';
-  // check if duplicat
-  const isDuplicate = todos.some(
-    (todo) => todo.value.toUpperCase() === todoValue.toUpperCase(),
-  );
-
-  if (isEmpty) {
-    alert('input a todo');
-  } else if (isDuplicate) {
-    alert('Todo Already Exist');
-  } else {
-    const todo = {
-      value: todoValue,
-      checked: false,
-      color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
-    };
-    todos.push(todo);
-    todoInput.value = '';
-    console.log(todos);
-  }
-}
 // renderTodo();
 // render todo
 function renderTodo() {
   // clear befor a re render
-  todoListEl.innerHTML = '';
+  todoListEl.innerHTML = "";
   todos.forEach((todo, index) => {
     todoListEl.innerHTML += `
     <div class="todo" id=${index}>
      <i 
-     class="bi ${todo.checked ? 'bi-check-lg' : 'bi-circle'}"
+     class="bi ${todo.checked ? "bi-check-lg" : "bi-circle"}"
       style="color:${todo.color}"
       data-action="check"
       >
       </i> 
-     <p class="${todo.checked ? 'checked' : ''}" data-action="check">${
-  todo.value
-}</p> 
+     <p class="${todo.checked ? "checked" : ""}" data-action="check">${
+      todo.value
+    }</p> 
     <i class="bi bi-trash" data-action="delete"></i> 
 </div>`;
   });
 }
 // form submit
-addContent.addEventListener('submit', (event) => {
+addContent.addEventListener("submit", (event) => {
   event.preventDefault();
   // eslint-disable-line no-use-before-define
   saveTodo();
   renderTodo();
-  localStorage.setItem('todos', JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
 });
 
 // check to do
@@ -70,7 +45,7 @@ function checkTodo(todoId) {
     checked: index === todoId ? !todo.checked : todo.checked,
   }));
   renderTodo();
-  localStorage.setItem('todos', JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 // delete to do
@@ -79,14 +54,14 @@ function deleteTodo(todoId) {
 
   // re render
   renderTodo();
-  localStorage.setItem('todos', JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 // click event listener for all todos
-todoListEl.addEventListener('click', (event) => {
+todoListEl.addEventListener("click", (event) => {
   const { target } = event;
   const parentElement = target.parentNode;
-  if (parentElement.className !== 'todo') return;
+  if (parentElement.className !== "todo") return;
 
   // todo id
   const todo = parentElement;
@@ -95,13 +70,26 @@ todoListEl.addEventListener('click', (event) => {
   // target action
   const { action } = target.dataset;
 
-  if (action === 'check') {
+  if (action === "check") {
     checkTodo(todoId);
   }
-  if (action === 'delete') {
+  if (action === "delete") {
     deleteTodo(todoId);
   }
 });
-window.addEventListener('load', () => {
+
+// clear all completed tasks from the lis
+const completeSec = document.querySelector(".clear-cont");
+completeSec.addEventListener("click", (event) => {
+  event.preventDefault();
+  const remain = todos.filter((ele) => ele.checked === false);
+  remain.forEach((e, i) => {
+    e.index = i;
+  });
+  localStorage.setItem("todos", JSON.stringify(remain));
+  window.location.reload();
+});
+
+window.addEventListener("load", () => {
   renderTodo();
 });
---
+++ src/modules/saveTodo.js
@@ -0,0 +1,28 @@
+import { todos } from "../app.js";
+const todoInput = document.querySelector("#newTodo");
+// save todo
+export default function saveTodo() {
+  const todoValue = todoInput.value;
+
+  // check if todo id empty
+  const isEmpty = todoValue === "";
+  // check if duplicat
+  const isDuplicate = todos.some(
+    (todo) => todo.value.toUpperCase() === todoValue.toUpperCase()
+  );
+
+  if (isEmpty) {
+    alert("input a todo");
+  } else if (isDuplicate) {
+    alert("Todo Already Exist");
+  } else {
+    const todo = {
+      value: todoValue,
+      checked: false,
+      color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+    };
+    todos.push(todo);
+    todoInput.value = "";
+    console.log(todos);
+  }
+}
